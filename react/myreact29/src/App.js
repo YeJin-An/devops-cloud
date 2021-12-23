545f5dbf33e800339aa7c5e9e0e85d20fc42c0d8
안예진 <ahw24033@naver.com>
2021-12-23 13:45:20 +0900

    전역변수

--- react/myreact29/src/App.js
+++ react/myreact29/src/App.js
@@ -1,9 +1,11 @@
 import Counter from './Counter';
+import MelonTop10 from './MelonTop10';
 
 function App() {
   return (
     <div>
       <h1> 안녕 리액트 </h1>
+      <MelonTop10 />
       <Counter />
       <Counter />
       <Counter />
