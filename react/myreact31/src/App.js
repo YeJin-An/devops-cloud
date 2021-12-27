b98739deb66f76f49f0174c0df3d5b2066c17b39
안예진 <ahw24033@naver.com>
2021-12-27 15:12:39 +0900

    react 컴포넌트 구성1

--- react/myreact31/src/App.js
+++ react/myreact31/src/App.js
@@ -1,27 +1,8 @@
 import React from "react";
-import Footer from "./Footer";
-import Header from "./Header";
+import PageLotto from "./pages/components/PageLotto";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Footer />
-        <Header />
-      </div>
-    );
-  }
+function App() {
+  return <PageLotto />;
 }
-
-App.defaultProps = {
-  number: 10,
-  number: 11,
-  number: 12,
-  number: 13,
-  number: 14,
-  number: 15,
-  number: 16,
-  number: 17,
-};
 
 export default App;
--- react/myreact31/src/pages/components/PageLotto.js
+++ react/myreact31/src/pages/components/PageLotto.js
@@ -1,13 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 
-class PageLotto extends Component {
-  render() {
-    return (
+function LottoList() {
+  const number = [
+    { number: 10 },
+    { number: 11 },
+    { number: 12 },
+    { number: 13 },
+    { number: 14 },
+    { number: 15 },
+    { number: 16 },
+    { number: 17 },
+  ];
+  return (
+    <div>
       <div>
-        <p>숫자: {this.props.number}</p>
+        <h4>숫자: {number[0].number}</h4>
       </div>
-    );
-  }
+      <div>
+        <h4>숫자: {number[1].number}</h4>
+      </div>
+      <div>
+        <h4>숫자: {number[2].number}</h4>
+      </div>
+      <div>
+        <h4>숫자: {number[3].number}</h4>
+      </div>
+      <div>
+        <h4>숫자: {number[4].number}</h4>
+      </div>
+      <div>
+        <h4>숫자: {number[5].number}</h4>
+      </div>
+      <div>
+        <h4>숫자: {number[6].number}</h4>
+      </div>
+      <div>
+        <h4>숫자: {number[7].number}</h4>
+      </div>
+    </div>
+  );
 }
 
-export default PageLotto;
+export default LottoList;
