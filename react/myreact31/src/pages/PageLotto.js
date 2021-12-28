c3273c05dc5e4094d078c934542485505002e863
안예진 <ahw24033@naver.com>
2021-12-28 13:54:05 +0900

    key값 설정

--- react/myreact31/src/pages/PageLotto.js
+++ react/myreact31/src/pages/PageLotto.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageLotto = () => {
   const Lotto = [10, 11, 12, 13, 14, 15, 16, 17];
-  const LottoList = Lotto.map((lotto) => <h5>{lotto}</h5>);
+  const LottoList = Lotto.map((lotto, index) => <h5 key={index}>{lotto}</h5>);
   return <ul>{LottoList}</ul>;
 };
 
