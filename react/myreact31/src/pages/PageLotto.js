32c5014ecf34dde64e1f5a14351cfdeee3e9570c
안예진 <ahw24033@naver.com>
2021-12-28 15:01:13 +0900

    난수 사용_3

--- react/myreact31/src/pages/PageLotto.js
+++ react/myreact31/src/pages/PageLotto.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 
 const PageLotto = () => {
-  const [lotto, setlotto] = useState([10, 11, 12, 13, 14, 15, 16, 17]);
-  const lottoList = lotto.map((lotto, index) => <h5 key={index}>{lotto}</h5>);
+  const [lotto, setlotto] = useState([10, 11, 12, 13, 14, 15, 16]);
+  const number = lotto.map((x) => Math.floor(Math.random(x) * 101));
   return (
     <ul style={{ margin: "40px", padding: "40px" }}>
-      {lotto[0]} {lotto[1]} {lotto[2]} {lotto[3]} {lotto[4]} {lotto[5]}{" "}
-      {lotto[6]} {lotto[7]}
+      <h2>{number}</h2>
     </ul>
   );
 };
