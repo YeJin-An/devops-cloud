9b3b70baa32b0cb8c98adbddf5dcffc34e58ed3c
안예진 <ahw24033@naver.com>
2021-12-28 13:39:16 +0900

    list 출력

--- react/myreact31/src/pages/PageLotto.js
+++ react/myreact31/src/pages/PageLotto.js
@@ -1,27 +1,24 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 function PageLotto() {
-  const number = [
-    { number: 10 },
-    { number: 11 },
-    { number: 12 },
-    { number: 13 },
-    { number: 14 },
-    { number: 15 },
-    { number: 16 },
-    { number: 17 },
-  ];
+  const [state, setState] = useState([]);
 
-  const [Lotto, setLotto] = useState();
-
-  function onClick() {
-    return setLotto(Math.floor(Math.random() * 101));
-  }
+  const setStateWithArray = () => {
+    const LottoList = [];
+    [10, 11, 12, 13, 14, 15, 16, 17].forEach((value) => {
+      LottoList.push(value);
+    });
+    setState(LottoList);
+  };
 
   return (
-    <div class="wrapper">
-      <h2>{`${Lotto} ${Lotto} ${Lotto} ${Lotto} ${Lotto} ${Lotto} ${Lotto} ${Lotto}`}</h2>
-      <button onClick={onClick}>예지</button>
+    <div className="wrapper">
+      <div>
+        {state?.map((value, idx) => (
+          <div key={idx}>{value}</div>
+        ))}
+      </div>
+      <button onClick={() => setStateWithArray()}>예지</button>
     </div>
   );
 }
