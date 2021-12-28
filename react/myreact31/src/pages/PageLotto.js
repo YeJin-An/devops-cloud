e011e3c4307cd227cc92a2c2ee782920823d248a
안예진 <ahw24033@naver.com>
2021-12-28 13:46:59 +0900

    state이란

--- react/myreact31/src/pages/PageLotto.js
+++ react/myreact31/src/pages/PageLotto.js
@@ -1,26 +1,22 @@
-import { useState } from "react";
-
-function PageLotto() {
-  const [state, setState] = useState([]);
-
-  const setStateWithArray = () => {
-    const LottoList = [];
-    [10, 11, 12, 13, 14, 15, 16, 17].forEach((value) => {
-      LottoList.push(value);
-    });
-    setState(LottoList);
-  };
-
-  return (
-    <div className="wrapper">
-      <div>
-        {state?.map((value, idx) => (
-          <div key={idx}>{value}</div>
-        ))}
+class pageLotto extends Component {
+  constructor(props) {
+    // constructor() 컴포넌트의 초기화
+    super(props);
+    this.state = {
+      Subject: { title: "WEB", sub: "월드와이드웹!" },
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Subject
+          title={this.statw.subject.title}
+          // 중괄호를 통해 State값을 가져온다.
+          sub={this.state.subject.sub}
+        ></Subject>
       </div>
-      <button onClick={() => setStateWithArray()}>예지</button>
-    </div>
-  );
+    );
+  }
 }
 
-export default PageLotto;
+// 캡슐화를 통해 코드를 리펙토링 하는 것이 좋은 사용성을 만드는 핵심
