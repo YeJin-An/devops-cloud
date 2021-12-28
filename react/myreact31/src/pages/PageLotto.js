2acf391c7e235c64085435030e3c3874ff52f256
안예진 <ahw24033@naver.com>
2021-12-28 16:30:14 +0900

    style-margin , clicked

--- react/myreact31/src/pages/PageLotto.js
+++ react/myreact31/src/pages/PageLotto.js
@@ -3,15 +3,20 @@ import React, { Component, useState } from "react";
 function PageLotto() {
   const [numbers, setNumbers] = useState([10, 11, 12, 13, 14, 15, 16]);
 
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
   return (
     <div>
       <ul>
         {numbers.map((number) => (
-          <h5>{number}</h5>
+          <span style={{ margin: "10px" }}>{number}</span>
         ))}
       </ul>
+      <button onClick={handleClick}>예지</button>
     </div>
   );
 }
 
-export default PageLotto;
+export default PageLotto; // 티스토리 144
