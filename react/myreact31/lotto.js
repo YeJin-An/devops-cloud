7b79586d757866286c23571ba8e22f11d70e6ae1
안예진 <ahw24033@naver.com>
2021-12-28 17:17:41 +0900

    lotto added

---
+++ react/myreact31/lotto.js
@@ -0,0 +1,18 @@
+let lotto = [];
+
+for (let i = 0; i < 6; i++) {
+  let num = Math.floor(Math.random() * 44) + 1;
+
+  for (let j in lotto) {
+    if (num == lotto[j]) {
+      num = Math.floor(Math.random() * 44) + 1;
+    }
+  }
+  lotto.push(num);
+}
+
+lotto.sort(function (a, b) {
+  return a - b;
+});
+
+console.log("이번주 로또 번호: " + lotto);
--- react/myreact31/src/pages/PageLotto.js
+++ react/myreact31/src/pages/PageLotto.js
@@ -1,9 +1,31 @@
 import React, { Component, useState } from "react";
 
+function makeLottoNumbers() {
+  let lotto = [];
+
+  for (let i = 0; i < 6; i++) {
+    let num = Math.floor(Math.random() * 44) + 1;
+
+    for (let j in lotto) {
+      if (num == lotto[j]) {
+        num = Math.floor(Math.random() * 44) + 1;
+      }
+    }
+    lotto.push(num);
+  }
+
+  lotto.sort(function (a, b) {
+    return a - b;
+  });
+
+  return lotto;
+}
+
 function PageLotto() {
   const [numbers, setNumbers] = useState([10, 11, 12, 13, 14, 15, 16]);
 
   const handleClick = () => {
+    setNumbers(makeLottoNumbers());
     console.log("clicked");
   };
 
