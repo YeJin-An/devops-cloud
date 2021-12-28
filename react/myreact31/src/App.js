380477e380e715c0c9872a21c14e606e9aea098e
안예진 <ahw24033@naver.com>
2021-12-28 13:52:54 +0900

    interface 출력

--- react/myreact31/src/App.js
+++ react/myreact31/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PageLotto from "./pages/PageLotto";
 // import "./styles/PageLotto.css";
 
--- react/myreact31/src/pages/PageLotto.js
+++ react/myreact31/src/pages/PageLotto.js
@@ -1,22 +1,9 @@
-class pageLotto extends Component {
-  constructor(props) {
-    // constructor() 컴포넌트의 초기화
-    super(props);
-    this.state = {
-      Subject: { title: "WEB", sub: "월드와이드웹!" },
-    };
-  }
-  render() {
-    return (
-      <div className="App">
-        <Subject
-          title={this.statw.subject.title}
-          // 중괄호를 통해 State값을 가져온다.
-          sub={this.state.subject.sub}
-        ></Subject>
-      </div>
-    );
-  }
-}
+import React from "react";
 
-// 캡슐화를 통해 코드를 리펙토링 하는 것이 좋은 사용성을 만드는 핵심
+const PageLotto = () => {
+  const Lotto = [10, 11, 12, 13, 14, 15, 16, 17];
+  const LottoList = Lotto.map((lotto) => <h5>{lotto}</h5>);
+  return <ul>{LottoList}</ul>;
+};
+
+export default PageLotto;
