97f202c8b41a9fc4df12e872a673593098335ff7
안예진 <ahw24033@naver.com>
2021-12-28 14:12:58 +0900

    random 난수 생성

--- react/myreact31/src/pages/PageLotto.js
+++ react/myreact31/src/pages/PageLotto.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 
 const PageLotto = () => {
-  const [Lotto, setLotto] = useState([10, 11, 12, 13, 14, 15, 16, 17]);
+  const [Lotto, setLotto] = useState([
+    Math.floor(Math.random() * 101),
+    Math.floor(Math.random() * 101),
+    Math.floor(Math.random() * 101),
+    Math.floor(Math.random() * 101),
+    Math.floor(Math.random() * 101),
+    Math.floor(Math.random() * 101),
+    Math.floor(Math.random() * 101),
+  ]);
   const LottoList = Lotto.map((lotto, index) => <h5 key={index}>{lotto}</h5>);
   return <ul>{LottoList}</ul>;
 };
