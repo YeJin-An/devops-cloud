a09d2b4898e2c7883638abd8ce86ec9f1126ce02
안예진 <ahw24033@naver.com>
2021-12-29 15:45:39 +0900

    React Props

--- react/myreact31/src/App.js
+++ react/myreact31/src/App.js
@@ -3,12 +3,17 @@ import ProfileCard from "./pages/ProfileCard";
 import DefaultProps from "./pages/test";
 // import "./styles/PageLotto.css";
 
-function App() {
-  return (
-    <>
-      <DefaultProps />
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          //<h1 title={this.props.title} id={this.props.id}>
+        }
+        <h1 {...this.props}>Hello, {this.props.frameworkName} world react!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
