f531a606739c5f4f0e066dfa3fafad6363838e42
안예진 <ahw24033@naver.com>
2021-12-29 14:39:26 +0900

    prop-types

--- react/myreact31/src/pages/test.js
+++ react/myreact31/src/pages/test.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 // prop-types [ yarn add prop-types ]
 import PropTypes from "prop-types";
+import App from "../App";
 
 // 기본값 지정
 class DefaultProps extends Component {
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return (
+      <>
+        <div>
+          <h1 {...this.props}>Hello, {this.props.frameworkName} world!</h1>
+        </div>
+      </>
+    );
   }
 }
+
+App.propTypes = {
+  title: PropTypes.string,
+  id: PropTypes.string,
+  frameworkName: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func,
+};
 
 export default DefaultProps;
