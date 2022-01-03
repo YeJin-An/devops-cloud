c6e19075a286a9fd6a9baf83efa056e9f6af7156
안예진 <ahw24033@naver.com>
2022-01-03 14:34:59 +0900

    Counter3 _color1

--- react/myreact32/src/pages/Counter3.jsx
+++ react/myreact32/src/pages/Counter3.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 
 function Counter3({ name }) {
-  const [value, setvalue] = useState(0);
+  const [state, setstate] = useState({ value: 0, color: 'lightpink' });
+  const { value, color } = state;
 
   const handleplus = (prevValue) => {
-    setvalue((prevValue) => prevValue + 1);
+    setstate((prevValue) => prevValue + 1);
   };
   const handleMinus = (prevValue) => {
-    setvalue((prevValue) => prevValue - 1);
+    setstate((prevValue) => prevValue - 1);
   };
   return (
     <>
-      <h1>{value}</h1>
+      <h1 style={{ backgroundColor: color }}>{value}</h1>
       <button onClick={handleplus}>+</button>
       <button onClick={handleMinus}>-</button>
     </>
