8529c8ddd8480b88bf5e9685080bd91e947097c5
안예진 <ahw24033@naver.com>
2022-01-03 10:52:02 +0900

    value setvalue _2

--- react/myreact32/src/pages/Conter.jsx
+++ react/myreact32/src/pages/Conter.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function Counter({ initialValue }) {
   const [value, setvalue] = useState(10);
+
+  function handleClick(prevValue) {
+    setvalue(prevValue + 1);
+  }
   return (
     <>
       <h1>Counter</h1>
       {value}
+      <hr />
+      <button onClick={handleClick}>+</button>
     </>
   );
 }
