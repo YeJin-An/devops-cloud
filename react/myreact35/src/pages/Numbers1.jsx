88c8ea1e24fa4d77025c4d2263a26090900d09cc
안예진 <ahw24033@naver.com>
2022-01-04 12:38:59 +0900

    step 3

--- react/myreact35/src/pages/Numbers1.jsx
+++ react/myreact35/src/pages/Numbers1.jsx
@@ -1,7 +1,52 @@
+import { useState } from "react";
+import Circle from "Circle";
+import { zip, get_random_lotto_numbers, shuffle_array } from "utils";
+
+const INTIAL_STATE = {
+  numbers: [0, 0, 0, 0, 0, 0, 0],
+  colors: [
+    "#1B62BF",
+    "#1755A6",
+    "#37A647",
+    "#F29F05",
+    "#F23838",
+    "purple",
+    "pink",
+  ],
+};
+
 function Numbers1({ title }) {
+  const [state, setState] = useState(INTIAL_STATE);
+
+  const generateNumbers = () => {
+    setState((prevState) => ({
+      ...prevState,
+      numbers: get_random_lotto_numbers(),
+    }));
+  };
+  const shuffleNumbers = () => {
+    setState((prevState) => ({
+      ...prevState,
+      numbers: shuffle_array(prevState.numbers),
+    }));
+  };
+  const shuffleColors = () => {
+    setState((prevState) => ({
+      ...prevState,
+      colors: shuffle_array(prevState.colors),
+    }));
+  };
+
   return (
     <>
-      <div>{title && <h2>useState{title}</h2>}</div>
+      {title && <h2>useState{title}</h2>}
+      {zip(state.numbers, state.colors).map(([number, color]) => (
+        <Circle number={number} backgroundColor={color} />
+      ))}
+      <hr />
+      <button onClick={generateNumbers}>GENERATE_NUMBERS</button>
+      <button onClick={shuffleNumbers}>SHUFFLE_NUMBERS</button>
+      <button onClick={shuffleColors}>SHUFFLE_COLORS</button>
     </>
   );
 }
--- react/myreact35/src/pages/Numbers2.jsx
+++ react/myreact35/src/pages/Numbers2.jsx
@@ -1,7 +1,58 @@
+import { useReducer } from "react";
+import Circle from "Circle";
+import { zip, get_random_lotto_numbers, shuffle_array } from "utils";
+
+const INITAL_STATE = {
+  numbers: [0, 0, 0, 0, 0, 0, 0],
+  colors: [
+    "#1B62BF",
+    "#1755A6",
+    "#37A647",
+    "#F29F05",
+    "#F23838",
+    "purple",
+    "pink",
+  ],
+};
+
+function reducer(prevState, action) {
+  const { type } = action;
+  if (type === "GENERATE_NUMBERS") {
+    return {
+      ...prevState,
+      numbers: get_random_lotto_numbers(),
+    };
+  } else if (type === "SHUFFLE_NUMBERS") {
+    return {
+      ...prevState,
+      numbers: shuffle_array(prevState.numbers),
+    };
+  } else if (type === "SHUFFLE_COLORS") {
+    return {
+      ...prevState,
+      colors: shuffle_array(prevState.colors),
+    };
+  }
+}
+
 function Numbers2({ title }) {
+  const [state, dispatch] = useReducer(reducer, INITAL_STATE);
   return (
     <>
-      <div>{title && <h2>다른버전{title}</h2>}</div>
+      <div>{title && <h2>useReducer{title}</h2>}</div>
+      {zip(state.numbers, state.colors).map(([number, color]) => (
+        <Circle number={number} backgroundColor={color} />
+      ))}
+      <hr />
+      <button onClick={() => dispatch({ type: "GENERATE_NUMBERS" })}>
+        GENERATE_NUMBERS
+      </button>
+      <button onClick={() => dispatch({ type: "SHUFFLE_NUMBERS" })}>
+        SHUFFLE_NUMBERS
+      </button>
+      <button onClick={() => dispatch({ type: "SHUFFLE_COLORS" })}>
+        SHUFFLE_COLORS
+      </button>
     </>
   );
 }
