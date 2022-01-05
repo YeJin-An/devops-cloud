668ab3b46317001919432d1803af8d68e648f976
안예진 <ahw24033@naver.com>
2022-01-05 16:24:34 +0900

    TodoForm_2

--- react/mystudy37/src/pages/components/TodoForm.jsx
+++ react/mystudy37/src/pages/components/TodoForm.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 function TodoForm() {
+  const [fieldValues, setFieldValues] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // console.log("changed", name, value);
+
+    setFieldValues({
+      ...fieldValues,
+      [name]: value,
+    });
+  };
   return (
     <>
       <h2>TodoForm</h2>
+
+      <input type="text" onChange={handleChange} name="목표는" />
+      <select>
+        <option>One</option>
+        <option>Two</option>
+        <option>Three</option>
+      </select>
+
+      <hr />
+      {JSON.stringify(fieldValues)}
     </>
   );
 }
