de7f618ce1d782cdf76d768af6a869f7a233a96a
안예진 <ahw24033@naver.com>
2022-01-05 20:16:51 +0900

    input tag_상태변경

--- react/one/src/pages/InpputSample.js
+++ react/one/src/pages/InpputSample.js
@@ -1,12 +1,23 @@
 import React from "react";
 
+import { useState } from "react";
+
 function InputSample() {
+  const [text, setText] = useState("");
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+  const onReset = () => {
+    setText("");
+  };
+
   return (
     <>
-      <input />
-      <button>initial</button>
+      <input onChange={onChange} value={text} />
+      <button onClick={onReset}>initial</button>
       <div>
-        <b>값: </b>
+        <b>값: {text}</b>
       </div>
     </>
   );
