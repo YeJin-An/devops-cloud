2b7c89ffd334e70668876b82a9ac8940e5d5f76f
안예진 <ahw24033@naver.com>
2022-01-05 11:10:18 +0900

    Counter dispath _3

--- react/myreact37/src/pages/components/Counter.jsx
+++ react/myreact37/src/pages/components/Counter.jsx
@@ -1,27 +1,33 @@
 import { useState, useReducer } from 'react';
 import './Counter.css';
 
+const ACTION_TYPES = {
+  INCREASE: 'INCREASE',
+  DECREASE: 'DECREASE',
+};
+
 function dispath(action, prevState) {
   const { type, amount } = action;
-  if (type === 'plus') {
+  if (type === ACTION_TYPES.INCREASE) {
     return prevState + amount;
-  } else if (type === 'minus') {
+  } else if (type === ACTION_TYPES.DECREASE) {
     return prevState - amount;
   }
+  return prevState; //방어
 }
 
 function Counter() {
   const [value, setValue] = useReducer(dispath, 0);
 
   const handleplus = () => {
-    const action = { type: 'plus', amount: 1 };
+    const action = { type: ACTION_TYPES.INCREASE, amount: 1 };
     setValue((prevValue) => {
       return dispath(action, prevValue);
     });
   };
   const handleminus = (e) => {
     e.preventDefault();
-    const action = { type: 'minus', amount: 1 };
+    const action = { type: ACTION_TYPES.DECREASE, amount: 1 };
     setValue((prevValue) => {
       return dispath(action, prevValue);
     });
