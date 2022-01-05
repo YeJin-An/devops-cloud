7fe289d351bd59ae12bc009ebafa37bec09e95d3
안예진 <ahw24033@naver.com>
2022-01-05 20:06:44 +0900

    useState_동적인 값

--- react/one/src/pages/Counter.js
+++ react/one/src/pages/Counter.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Counter() {
+  const [number, setNumber] = useState(0);
+  const onIncreace = () => {
+    setNumber(number + 1);
+  };
+  const onDecrease = () => {
+    setNumber(number - 1);
+  };
+
   return (
     <>
-      <h1>0</h1>
-      <button>+1</button>
-      <button>-1</button>
+      <h1>{number}</h1>
+      <button onClick={onIncreace}>+1</button>
+      <button onClick={onDecrease}>-1</button>
     </>
   );
 }
