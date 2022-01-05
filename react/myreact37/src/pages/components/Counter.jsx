4b3463323eecdede979bc1d9f1e5e616a9842494
안예진 <ahw24033@naver.com>
2022-01-05 10:55:54 +0900

    Counter button _2

--- react/myreact37/src/pages/components/Counter.jsx
+++ react/myreact37/src/pages/components/Counter.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react';
 import './Counter.css';
 
+function reducer(action, prevState) {
+  const { type, amount } = action;
+  if (type === 'plus') {
+    return prevState + amount;
+  } else if (type === 'minus') {
+    return prevState + amount;
+  }
+}
+
 function Counter() {
   const [value, setValue] = useState(0);
 
   const handleplus = () => {
-    setValue(value + 1);
+    setValue((prevValue) => prevValue + 1);
   };
   const handleminus = () => {
-    setValue(value - 1);
+    setValue((prevValue) => prevValue + 1);
   };
   return (
     <>
