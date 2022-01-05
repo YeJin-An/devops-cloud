c7c67f0cbd48e157a4e54b5bb46667eb12b85abc
안예진 <ahw24033@naver.com>
2022-01-05 10:49:45 +0900

    Counter button _1

--- react/myreact37/src/App.jsx
+++ react/myreact37/src/App.jsx
@@ -3,7 +3,7 @@ import PageCounter from './pages/PageCounter';
 function App() {
   return (
     <>
-      <PageCounter />;
+      <PageCounter />
     </>
   );
 }
--- react/myreact37/src/pages/components/Counter.jsx
+++ react/myreact37/src/pages/components/Counter.jsx
@@ -3,13 +3,24 @@ import './Counter.css';
 
 function Counter() {
   const [value, setValue] = useState(0);
+
+  const handleplus = () => {
+    setValue(value + 1);
+  };
+  const handleminus = () => {
+    setValue(value - 1);
+  };
   return (
     <>
-      <div className="Counter" style={{ backgroundColor: 'lightpink' }}>
+      <div className="counter" style={{ backgroundColor: 'lightpink' }}>
         {value}
       </div>
+      <button onClick={handleplus}>+1</button>
+      <button onClick={handleminus}>-1</button>
     </>
   );
 }
 
 export default Counter;
+
+// 카운터를 클릭 시에 1씩 증가하도록 구현.
