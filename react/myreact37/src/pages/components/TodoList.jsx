ba434302fd0d977bda7183a0c15e6c66bb090ae1
안예진 <ahw24033@naver.com>
2022-01-05 11:21:41 +0900

    TodoList_1

--- react/myreact37/src/pages/components/TodoList.jsx
+++ react/myreact37/src/pages/components/TodoList.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
+const INITIAL_STATE = [
+  { contnet: '2022년에는 살빼기' },
+  { contnet: '파이썬 익히기' },
+  { contnet: '리액트 익히기' },
+];
+
 function TodoList() {
+  const [todoList, setTodoList] = useState(INITIAL_STATE);
+
   return (
     <>
       <h2>TodoList</h2>
+      {todoList.map((todo) => (
+        <div>{todo.contnet}</div>
+      ))}
     </>
   );
 }
