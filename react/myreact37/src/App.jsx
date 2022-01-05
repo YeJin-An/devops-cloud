c87e0bbb39465d5969e8063b2c64ef9c0fdf1bff
안예진 <ahw24033@naver.com>
2022-01-05 11:14:59 +0900

    TodoList 컴포넌트 생성

--- react/myreact37/src/App.jsx
+++ react/myreact37/src/App.jsx
@@ -1,9 +1,11 @@
 import PageCounter from './pages/PageCounter';
+import PageTodoList from './pages/PageTodoList';
 
 function App() {
   return (
     <>
       <PageCounter />
+      <PageTodoList />
     </>
   );
 }
